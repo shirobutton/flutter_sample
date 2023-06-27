6447c31e37fb747b931cad653dffa35bf41f1581
shirobutton <shirobutton@gmail.com>
2023-06-27 18:03:46 +0900

    apiServiceProviderを追加

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,8 +1,14 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_sample/post.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'api_service.g.dart';
+
+final apiServiceProvider = Provider<ApiService>((ref) {
+  final dio = Dio();
+  return ApiService(dio);
+});
 
 @RestApi(baseUrl: 'https://jsonplaceholder.typicode.com/')
 abstract class ApiService {
